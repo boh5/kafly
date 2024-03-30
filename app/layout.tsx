4e9a68a80de8f1530ed4e14e2175686f9ff0f274
boh5 <bohuang1994@gmail.com>
2024-03-30 18:47:44 +0800

    feat: add  theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter as FontSans } from "next/font/google"
 import "./globals.css"
 
 import { cn } from "@/lib/utils"
+import { ThemeProvider } from "@/components/theme-provider"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -28,7 +29,9 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
